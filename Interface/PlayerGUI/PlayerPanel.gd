36f7a37a621a04991cdc86dea4a25c249e64b74a
Dmitry Laletin <laletindn@mail.ru>
2019-12-02 21:59:00 +0300

    improve user interface

--- Interface/PlayerGUI/PlayerPanel.gd
+++ Interface/PlayerGUI/PlayerPanel.gd
@@ -2,7 +2,10 @@ extends MarginContainer
 
 var pl_ref = null
 var items_cnt = 0
-var inv_path = "Panel/HBoxContainer/VBoxContainer/HBoxContainer/InvContainer/Inv_"
+var inv_path = "Panel/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/Inv_%/Inv_%"
+var lab_path = "Panel/HBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer/Inv_%/Lab_%"
+
+var label_weight = 11
 
 var inventory_dict = {
 	torch = 0,
@@ -13,6 +16,8 @@ var cnt_dict = {}
 
 func _ready():
 	set_size(Vector2(OS.get_window_size().x, 64 + 32))
+	
+	UsingItemsLambdas.connect("del_item", self, "del_item")
 
 func setup(player_ref):
 	UsingItemsLambdas.pl_ref = player_ref
@@ -24,11 +29,32 @@ func get_item_pos(name):
 	return inventory_dict[name]
 
 func add_item(name, path):
+	var inv_node = get_node(inv_path.replace("%", str(get_item_pos(name))))
+	var lab_node = get_node(lab_path.replace("%", str(get_item_pos(name))))
 	if not name in cnt_dict or cnt_dict[name] == 0:
-		var inv_node = get_node(inv_path + str(get_item_pos(name)))
 		cnt_dict[name] = 1
 		inv_node.icon = load(path)
+		lab_node.text = "Кол-во: 1  "
 		inv_node.connect("pressed", UsingItemsLambdas, name)
 	else:
-		# increase cnt
-		pass
+		cnt_dict[name] += 1
+		
+		lab_node.text = align_text("Кол-во: " + str(cnt_dict[name]), label_weight)
+	
+func align_text(text, cnt):
+	while text.length() != cnt:
+		text += " "
+	return text
+	
+func del_item(name):
+	var inv_node = get_node(inv_path.replace("%", str(get_item_pos(name))))
+	var lab_node = get_node(lab_path.replace("%", str(get_item_pos(name))))
+	if name in cnt_dict:
+		cnt_dict[name] -= 1
+		if cnt_dict[name] == 0:
+			inv_node.icon = null
+			lab_node.text = "" # yes! 11 spaces
+			inv_node.disconnect("pressed", UsingItemsLambdas, name)
+		else:
+			lab_node.text = align_text("Кол-во: " + str(cnt_dict[name]), label_weight)
+			
